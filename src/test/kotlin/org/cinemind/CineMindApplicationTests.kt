3505a5604ee393ffc6f8d55860d11510f9d636b6
hanjsnote <enlun@naver.com>
2025-10-20 16:58:33 +0900

    feat: auth-intergretionTest(통합테스트) 보류

--- src/test/kotlin/org/cinemind/CineMindApplicationTests.kt
+++ src/test/kotlin/org/cinemind/CineMindApplicationTests.kt
@@ -2,8 +2,26 @@ package org.cinemind
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
+import org.springframework.test.context.TestPropertySource
+import javax.sql.DataSource
 
 @SpringBootTest
+@TestPropertySource(properties = [
+    // 💡 PostgreSQL 설정을 H2 설정으로 강제 오버라이드
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.url=jdbc:h2:mem:testdb;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+
+    // 💡 JWT 설정도 함께 오버라이드 (이전 @ActiveProfiles("test")의 역할)
+    "jwt.secret.key=VtptGZ/QLQ6zzknA91ILRzfjg9cfpuIt19jww+kXDTu/PO4fK12QU5g6dmNLKT1vdNo7PlbaE0J33GlhMq14WA=="
+])
+
+//@SpringBootTest
 class CineMindApplicationTests {
 
     @Test
