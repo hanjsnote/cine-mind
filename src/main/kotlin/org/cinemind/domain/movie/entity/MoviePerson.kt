a312fe72ae9d0a7a8c129fd7a6a4b9fb0c9d3825
hanjsnote <enlun@naver.com>
2025-10-21 22:27:10 +0900

    feat: movie 엔티티 구현

--- src/main/kotlin/org/cinemind/domain/movie/entity/MoviePerson.kt
+++ src/main/kotlin/org/cinemind/domain/movie/entity/MoviePerson.kt
@@ -8,12 +8,10 @@ import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
-import org.apache.catalina.Role
 import org.cinemind.common.entity.BaseEntity
 import org.cinemind.domain.movie.enums.PersonRole
 
 // Movie, Person 매핑 테이블
-// 배우(actor), 감독(director)은 role로 구분, cast는 배역(역할)
 @Entity
 @Table(name="movie_persons")
 class MoviePerson (
@@ -27,7 +25,7 @@ class MoviePerson (
     val person: Person,
 
     val role: PersonRole, // 감독(DIRECTOR), 배우(ACTOR)
-    val castNm: String     // 배역(역할)
+    val castNm: String     // 배역명
 
 ) : BaseEntity() {
 
