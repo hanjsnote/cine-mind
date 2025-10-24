26f0c59a1f8dd57ded98b07cf1f5c9c5b23fc09f
hanjsnote <enlun@naver.com>
2025-10-25 08:58:25 +0900

    feat: 데이터 적재 스케줄러 로직 구현

--- src/main/kotlin/org/cinemind/domain/kofic/service/KoficDataSyncService.kt
+++ src/main/kotlin/org/cinemind/domain/kofic/service/KoficDataSyncService.kt
@@ -22,6 +22,7 @@ import org.cinemind.domain.movie.repository.PeopleRepository
 import org.cinemind.util.DateUtils
 import org.cinemind.util.toLocalDate
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 
 @Service
 @Transactional
@@ -39,41 +40,53 @@ class KoficDataSyncService (
 ){
     // 전체 영화 목록을 조회
     fun saveMovieList() {
-        val itemPerPage = 100   // 한 번에 적재할 영화 갯수
+        val itemPerPage = 10   // 한 번에 적재할 영화 갯수
         var currentPage = 1     // 시작할 현재 페이지 번호
         var totalPages = 1
 
+        // 테스트로 100건만 저장
+        val result = koficApiClient.getMovieList(currentPage, itemPerPage)
+
+        if (result == null) {
+            println("Warning: Kofic API returned null for the first page.")
+            return
+        }
+
+        result.movieList.forEach { listItem ->
+            saveMovieData(listItem.movieCd)
+        }
+
         // 페이지네이션을 반복하여 전체 목록 확보
-        while (currentPage <= totalPages) {
-            val result = koficApiClient.getMovieList(currentPage, itemPerPage) ?: break
-
-            // 전체 페이지 수 계산 1151 페이지
-            if (currentPage == 1) {
-                totalPages = (result.totCnt + itemPerPage - 1) / itemPerPage
-            }
-
-            //확보된 목록을 순회하며 상세 정보 적재
-            result.movieList.forEach { listItem ->
-                // 목록 DTO를 사용하여 Movie 엔티티를 찾거나 생성/저장
-                saveMovieData(listItem.movieCd)
-            }
-            currentPage++
-        }
+//        while (currentPage <= totalPages) {
+//            val result = koficApiClient.getMovieList(currentPage, itemPerPage) ?: break
+//
+//            // 전체 페이지 수 계산 1151 페이지
+//            if (currentPage == 1) {
+//                totalPages = (result.totCnt + itemPerPage - 1) / itemPerPage
+//            }
+//
+//            //확보된 목록을 순회하며 상세 정보 적재
+//            result.movieList.forEach { listItem ->
+//                // 목록 DTO를 사용하여 Movie 엔티티를 찾거나 생성/저장
+//                saveMovieData(listItem.movieCd)
+//            }
+//            currentPage++
+//        }
     }
 
-    // targetDt를 이용해 BoxOffice 데이터 적재
-    fun startLoadProcess(startDate: String, endDate: String) {
-
-        // 날짜 범위 생성 및 반복 호출
-        val datesToLoad = dateUtils.generateDate(startDate, endDate)
-
-        datesToLoad.forEach { targetDt ->
-            // 단일 날짜의 BoxOffice 목록을 가져옴
-            val dailyBoxOfficeList = koficApiClient.getMovieBoxOffice(targetDt)
-            // 날짜 정보(targetDt)와 BoxOffice를 결합하여 처리
-//            saveBoxOffice(targetDt, dailyBoxOfficeList)
-        }
-    }
+//    // targetDt를 이용해 BoxOffice 데이터 적재
+//    fun startLoadProcess(startDate: String, endDate: String) {
+//
+//        // 날짜 범위 생성 및 반복 호출
+//        val datesToLoad = dateUtils.generateDate(startDate, endDate)
+//
+//        datesToLoad.forEach { targetDt ->
+//            // 단일 날짜의 BoxOffice 목록을 가져옴
+//            val dailyBoxOfficeList = koficApiClient.getMovieBoxOffice(targetDt)
+//            // 날짜 정보(targetDt)와 BoxOffice를 결합하여 처리
+////            saveBoxOffice(targetDt, dailyBoxOfficeList)
+//        }
+//    }
 
     // 영화 코드를 기준으로 DB에서 Movie 엔티티를 찾거나 없으면 Movie와 모든 매핑 엔티티를 저장
     private fun saveMovieData(movieCd: String): Movie? {
@@ -100,12 +113,15 @@ class KoficDataSyncService (
 
     // Movie 엔티티 저장 로직 MovieInfoResponse를 Movie 엔티티로 변환 후 저장
     private fun saveMovie(movieInfo: MovieInfo): Movie {
+
+        val openDate = movieInfo.openDt.toLocalDate() ?: LocalDate.of(1990, 1, 1)
+
         return movieRepository.save(Movie(
             movieCd = movieInfo.movieCd,
-            movieNm = movieInfo.movieCd,
+            movieNm = movieInfo.movieNm,
             movieNmEn = movieInfo.movieNmEn,
             showTm = movieInfo.showTm.toIntOrNull() ?: 0,
-            openDt = movieInfo.openDt.toLocalDate(),
+            openDt = openDate,
             typeNm = movieInfo.typeNm,
             watchGradeNm = movieInfo.audits.firstOrNull()?.watchGradeNm ?: "전체 관람가"
         ))
@@ -139,9 +155,5 @@ class KoficDataSyncService (
                 ?: companyRepository.save(Company(companyDto.companyCd, companyDto.companyNm, companyDto.companyNmEm ?: "" ))
             movieCompanyRepository.save(MovieCompany(movie = movie, company = company, companyPartNm = companyDto.companyPartNm))
         }
-
-
-
     }
-
 }
