ffda1b064715914db7eb3665be5c68dbd80806e5
hanjsnote <enlun@naver.com>
2025-11-05 11:02:30 +0900

    feat: kmdb-유사도 점수 디버깅 로직 구현

--- src/main/kotlin/org/cinemind/domain/externalApis/dto/etc/MatchDetail.kt
+++ src/main/kotlin/org/cinemind/domain/externalApis/dto/etc/MatchDetail.kt
@@ -4,7 +4,7 @@ import org.cinemind.domain.externalApis.dto.response.KmdbResult
 
 class MatchDetail (
     val kmdbResult: KmdbResult,         // KMDb 원본데이터
-    val normalizedkmdbTitle: String,    // 정규화된 KMDB 제목
+    val normalizedKmdbTitle: String,    // 정규화된 KMDB 제목
     val titleScore: Double,             // 제목 유사도 점수 (Jaro-winkler)
     val totalScore: Double,             // 최종 총 점수 (유사도 + 연도/보너스)
     val yearMatch: Boolean,             // 연도 일치 여부
