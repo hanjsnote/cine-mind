cc34234ba87b708cc11f157144fa984c6f895e53
hanjsnote <enlun@naver.com>
2025-12-21 22:45:36 +0900

    ref:스케줄러 변경

--- src/main/kotlin/org/cinemind/scheduler/KoficDataLoadScheduler.kt
+++ src/main/kotlin/org/cinemind/scheduler/KoficDataLoadScheduler.kt
@@ -5,6 +5,7 @@ import org.cinemind.domain.externalApis.service.KoficDataSyncService
 import org.springframework.context.annotation.Profile
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 // 적재 로직 실행 스케줄러
@@ -15,9 +16,9 @@ class KoficDataLoadScheduler (
     private val boxOfficeSyncService: BoxOfficeSyncService
 ) {
     // 영화 목록 적재 스케줄링 (월 1회: 매월 1일 새벽 2시)
-//    @Scheduled(cron = "0 0 2 1 * ?")
+    @Scheduled(cron = "0 0 2 1 * ?")
     // 애플리케이션 시작 후 바로 적재 시작
-    @EventListener(ContextRefreshedEvent::class)
+//    @EventListener(ContextRefreshedEvent::class)
     fun initialLoadData() {
         println("Starting scheduled movie list load...")
         koficDataSyncService.saveMovieList()
